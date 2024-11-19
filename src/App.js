b5fc2b437d0754dfbdba7d00d8758cb8a9435856
nitinshukla <nitin.shukla12004@gmail.com>
2024-11-19 20:16:03 +0530

    initial commit: installed dependencies and configured tailwind for styling

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='font-bold text-center text-green-400 text-3xl'>Hello Netflix - GPT</p>
     </div>
   );
 }
